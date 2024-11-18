4e825a6331a3e6ad79f7fc1a4064fc1330b7c466
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2024-11-18 11:30:30 +0100

    feat: fixes env config

--- config/db.go
+++ config/db.go
@@ -9,22 +9,15 @@ import (
 	"github.com/ayoadeoye1/restapi-gin-gorm/helper"
 )
 
-// if err := godotenv.Load(); err != nil {
-// 	log.Fatalf("Error loading .env file")
-// }
-
-var (
-	host     = EnvFile["DB_HOST"]
-	port     = EnvFile["DB_PORT"]
-	user     = EnvFile["DB_USER"]
-	password = EnvFile["DB_PASSWORD"]
-	dbName   = EnvFile["DBNAME"]
-)
-
 func ConnectDB() *gorm.DB {
+	host := EnvFile["DB_HOST"]
+	port := EnvFile["DB_PORT"]
+	user := EnvFile["DB_USER"]
+	password := EnvFile["DB_PASSWORD"]
+	dbName := EnvFile["DBNAME"]
+
 	dbConfig := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", host, port, user, password, dbName)
 	db, err := gorm.Open(postgres.Open(dbConfig), &gorm.Config{})
-	fmt.Println(dbConfig, ";;;;;::::;;:::")
 	helper.ErrorPanic(err)
 
 	fmt.Println("DB Connection Successful")
--- config/load-env.go
+++ config/load-env.go
@@ -1,7 +1,26 @@
 package config
 
+// import (
+// 	"log"
+
+// 	"github.com/joho/godotenv"
+// )
+
+// var EnvFile map[string]string
+
+// func init() {
+// 	var err error
+// 	EnvFile, err = godotenv.Read("../.env")
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file: %v", err)
+// 	}
+// }
+// package config
+
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
@@ -9,8 +28,14 @@ import (
 var EnvFile map[string]string
 
 func init() {
-	var err error
-	EnvFile, err = godotenv.Read("./.env")
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatalf("Error determining directory: %v", err)
+	}
+
+	envPath := filepath.Join(dir, "../.env")
+
+	EnvFile, err = godotenv.Read(envPath)
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}

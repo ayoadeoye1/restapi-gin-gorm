248f36f66823b1b4680133c0450233f0a5209feb
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2024-12-09 11:50:59 +0100

    feat: fixing rm old code

--- repository/user_repository/user_repo_intf.go
+++ repository/user_repository/user_repo_intf.go
@@ -2,14 +2,6 @@ package userrepository
 
 import "github.com/ayoadeoye1/restapi-gin-gorm/models"
 
-//	type UserRepo interface {
-//		Add(users models.Users) error
-//		Edit(users models.Users) error
-//		FindAll(userId int) []models.Users
-//		FindById(userId int) (users models.Users, err error)
-//		FindByEmail(userEmail string) (users models.Users, err error)
-//		Remove(userId int) error
-//	}
 type UserRepo interface {
 	Add(users models.Users) error
 	Edit(users models.Users) error
@@ -18,12 +10,3 @@ type UserRepo interface {
 	FindById(userId int) (*models.Users, error)
 	Remove(userId int) error
 }
-
-// type UserRepo interface {
-// 	Add(users models.Users) error
-// 	Edit(users models.Users) error
-// 	FindAll(userId int) ([]models.Users, error)
-// 	FindById(userId int) (*models.Users, error)
-// 	FindByEmail(userEmail string) (*models.Users, error)
-// 	Remove(userId int) error
-// }
